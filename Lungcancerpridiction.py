294e8d9e40266afcac01b0a99aa4096a6f9d6ee6
IkmalFazli <102709684+IkmalFazli@users.noreply.github.com>
2022-04-04 14:50:34 +0800

    Update Lungcancerpridiction.py

--- Lungcancerpridiction.py
+++ Lungcancerpridiction.py
@@ -1,162 +1,123 @@
 import streamlit as st
+import numpy as np 
 import pandas as pd
-import pickle
-import requests
-import numpy as np
-from bs4 import BeautifulSoup 
-import nltk
+from sklearn.preprocessing import LabelEncoder
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
 
-print('\nBe patience downloading the files will take time....\n')
-nltk.download('punkt')
 
-print('punkt downloading completed!\n')
+st.title('DID YOU REALLY SAVE FROM LUNG CANCER?')
 
-nltk.download('stopwords')
-print('\nstopwords downloading completed! Application is running.')
+st.sidebar.write("""
+This is a web app demo using python libraries such as Streamlit, Sklearn etc
+""")
 
-header={'Accept':'application/vnd.github.mercy-preview+json',
-'visibility':'PUBLIC',
-"Authorization": "token PASTE_YOUR_GITHUB_TOKEN_HERE"
-}
+st.sidebar.write ("For more info, please contact:")
 
-st.markdown(f'''<h1 style="text-align:center;color:#929aab;font-family:Bitter;font-size:54px;margin-bottom:20px">
-                Github Repo Popularity <br>Prediction</h1>''', unsafe_allow_html=True)
+st.sidebar.write("<a href='www.linkedin.com/in/ikmal-fazli-876621234'>Mr. Ikmal Fazli</a>", unsafe_allow_html=True)
 
-st.image('media/original.gif')
+choice = st.sidebar.radio(
+    "Choose a model to predict:",   
+    ('Random Forest','KNN Neughbors', 'support vector machine','logistic regression','Gaussian Naive Bayes'),
+    index = 0
+)
 
-# loading the vocabulary and the train pickle file
-with open('pretrained_model/objs.pkl','rb') as f:  
-    store_tag, vocabulary, vocab, word_dict = pickle.load(f)
-with open('pretrained_model/train_model.pkl','rb') as f: 
-    model = pickle.load(f)
 
-# print(vocabulary)
-def perc(tags):
-    """This function returns the ratio of the total of tags in repository
-    Args:
-        tags (List): Tags that are in the repository
-    Returns:
-        sum_of_perc*100 [float]: 
-    """
-    sum_of_perc=0
-    
-    for tag in tags:
-        # print(tag)
-        if tag in store_tag:
-            sum_of_perc += (store_tag[tag]/vocabulary)
-    
-    return (sum_of_perc*100)
 
-def string_num(text):
-    """Mapping the word with the corresponding number
-    Args:
-        text (List): List of words
-    Returns:
-        string_numbers (int): Total of mapped integer corresponding to the words in text
-    """
-    string_numbers = 0
-    for string in text:
-        if string in word_dict:
-            string_numbers += word_dict[string]
-    return string_numbers
-
-st.sidebar.markdown('''This is the project to predict the repo popularity using No of **'Forks'**, No of **'Watcher'**, No of **'Issues'**,
-                    No of **'Tags'**, **'Description of Repo'**, No of **'Contributers'**, **'License'** and the **'Languages used in project'**.
-                    The data has been collected using BeautifulSoap, GitHub API and from Kaggle GitHub datasets, then Machine learning 
-                    Regression is used to predict the Repo Popularity.''')
-
-keys=['forks','watchers','open_issues_count','topics','description','language']
 form = st.form(key='my_form')
-user_name = form.text_input("Username")
-repo_name= form.text_input("Repo name")
+user_name = form.text_input("Name")
+Age = form.text_input("Age")
+gender= form.text_input("Gender(M for Male, F for Female)")
+smok= form.text_input("Currently smoking?(YES/NO)")
+yf= form.text_input("Yellow Fingers? (YES/NO)")
+Anx= form.text_input("Having anxiety recently?(YES/NO)")
+PP= form.text_input("Pear-pressure?(YES/NO)")
+cd= form.text_input("Any Chronic disease?(YES/NO)")
+Fat= form.text_input("Fatique?(YES/NO)")
+All= form.text_input("Any Allergy reaction?(YES/NO)")
+whe= form.text_input("Wheeing?(YES/NO)")
+Alc= form.text_input("Any alcohol consuming?(YES/NO)")
+cough= form.text_input("Coughing?(YES/NO)")
+Sho= form.text_input("Having Shortness of breath?(YES/NO)")
+swa= form.text_input("Any difficulty to swallow?(YES/NO)")
+cp= form.text_input("Any Chest pain?(YES/NO)")
+
 submit_button = form.form_submit_button(label='Predict')
 
 if submit_button:
-    try:
-        user_name=user_name.strip()
-        repo_name=repo_name.strip()
-        request=requests.get('https://api.github.com/repos/'+user_name+'/'+repo_name,headers=header)
-        data=request.json()
-        # print(data)
-        
-        dataf=[data[k] for k in keys]
-        if str(data['license']).lower()=='none':
-            license=0
-        else:
-            license=1
-            
-        request=requests.get('https://api.github.com/repos/'+user_name+'/'+repo_name+'/contributors',headers=header)
-        dat=request.json()
-        contributors=len(dat)
-        dataf.append(contributors)
-        dataf.append(license)
-        df=pd.DataFrame([dataf],columns=['fork','watch','issue','tags','description','most_used_lang','contributers','license'])
-        
-        df['most_used_lang']=df['most_used_lang'].str.lower()
-        df['description']=df['description'].str.lower()
-        df['tag_ratio']=df['tags'].apply(perc).astype(float)
-        
-        if df['description'][0]:
-            text=nltk.word_tokenize(df['description'][0])
-            df['desc_to_num'] = string_num(text)
-        else:
-            df['desc_to_num']=0
-        
-        word_counts_for_language = {unique_word: [0] for unique_word in vocab}
-        
-        df['most_used_lang']=df['most_used_lang'].str.split()
-        for word in df['most_used_lang']:
-            if word[0] in word_counts_for_language:
-                word_counts_for_language[word[0].lower()][0] += 1
-        
-        first_kpi, second_kpi, third_kpi,fourth= st.beta_columns(4)
+         heart_data2 = pd.DataFrame(columns = ['GENDER','AGE', 'SMOKING', 'YELLOW_FINGERS', 'ANXIETY', 'PEER_PRESSURE', 'CHRONIC DISEASE', 'FATIGUE', 'ALLERGY', 'WHEEZING',  'ALCOHOL CONSUMING', 'COUGHING', 'SHORTNESS OF BREATH', 'SWALLOWING DIFFICULTY', 'CHEST PAIN' ])
+         heart_data2 = heart_data2.append({'GENDER':gender,'AGE':Age, 'SMOKING':smok, 'YELLOW_FINGERS':yf, 'ANXIETY':Anx, 'PEER_PRESSURE':PP, 'CHRONIC DISEASE':cd, 'FATIGUE':Fat, 'ALLERGY':All, 'WHEEZING':whe,  'ALCOHOL CONSUMING':Alc, 'COUGHING':cough, 'SHORTNESS OF BREATH':sho, 'SWALLOWING DIFFICULTY':swa, 'CHEST PAIN':cp}, ignore_index = True)
+         data = pd.read_csv('survey lung cancer.csv', index_col = False)
+         labelencoder1 = LabelEncoder()
+         labelencoder2 = LabelEncoder()
+         labelencoder3 = LabelEncoder()
+         labelencoder4 = LabelEncoder()
+         labelencoder5 = LabelEncoder()
+         labelencoder6 = LabelEncoder()
+         labelencoder7 = LabelEncoder()
+         labelencoder8 = LabelEncoder()
+         labelencoder9 = LabelEncoder()
+         labelencoder10 = LabelEncoder()
+         labelencoder11 = LabelEncoder()
+         labelencoder12 = LabelEncoder()
+         labelencoder13 = LabelEncoder()
+         labelencoder14 = LabelEncoder()
 
+         data['GENDER'] = labelencoder1.fit_transform(data['GENDER'])
+         data['SMOKING'] = labelencoder2.fit_transform(data['SMOKING'])
+         data['YELLOW_FINGERS'] = labelencoder3.fit_transform(data['YELLOW_FINGERS'])
+         data['ANXIETY'] = labelencoder4.fit_transform(data['ANXIETY'])
+         data['PEER_PRESSURE'] = labelencoder5.fit_transform(data['PEER_PRESSURE'])
+         data['CHRONIC DISEASE'] = labelencoder6.fit_transform(data['CHRONIC DISEASE'])
+         data['FATIGUE '] = labelencoder7.fit_transform(data['FATIGUE '])
+         data['ALLERGY '] = labelencoder8.fit_transform(data['ALLERGY '])
+         data['WHEEZING'] = labelencoder9.fit_transform(data['WHEEZING'])
+         data['ALCOHOL CONSUMING'] = labelencoder10.fit_transform(data['ALCOHOL CONSUMING'])
+         data['COUGHING'] = labelencoder11.fit_transform(data['COUGHING'])
+         data['SHORTNESS OF BREATH'] = labelencoder12.fit_transform(data['SHORTNESS OF BREATH'])
+         data['SWALLOWING DIFFICULTY'] = labelencoder13.fit_transform(data['SWALLOWING DIFFICULTY'])
+         data['CHEST PAIN'] = labelencoder14.fit_transform(data['CHEST PAIN'])
 
-        with first_kpi:
-            st.markdown("**Issues**")
-            number1 = data['open_issues']
-            st.markdown(f"<h1 style=' color: #BAE8E8;'>{number1}</h1>", unsafe_allow_html=True)
-        
-        with second_kpi:
-            st.markdown("**Most Used Language**")
-            number2 = data['language']
-            st.markdown(f"<h1 style=' color: #BAE8E8;'>{number2}</h1>", unsafe_allow_html=True)
-        
-        with third_kpi:
-            st.markdown("**Forks**")
-            number3 = data['forks']
-            st.markdown(f"<h1 style=' color: #BAE8E8;'>{number3}</h1>", unsafe_allow_html=True)
+         X = data.drop(['LUNG_CANCER'], axis = 1)
+         y = data['LUNG_CANCER']
 
-        with fourth:
-            st.markdown("**TAGs**")
-            if data['topics']:
-                number4 = data['topics']
-            else:
-                number4='Not mentioned'
-            st.markdown(f"<h1 style=' color: #BAE8E8;'>{number4}</h1>", unsafe_allow_html=True)
+         if choice == 'Random Forest':
+             RandomForest = RandomForestClassifier(n_estimators=10000, max_depth=15)
+             RandomForest.fit(Xtrain, ytrain)
+             ypred = RandomForest.predict(heart_data2)
+             if ypred == '0':
+                 st.write("You might have lung cancer, go get a doctor now!)
+             else:
+                 st.write("You're as healthy as a horse")
 
-        st.markdown(f"<hr>", unsafe_allow_html=True)
-        first, sec= st.beta_columns(2)
+         elif choice == 'KNN Neughbors':
+             knn = KNeighborsClassifier()
+             knn.fit(X, y)
+             ypred = knn.predict(heart_data2)
+             if ypred == '0':
+                 st.write("You might have lung cancer, go get a doctor now!)
+             else:
+                 st.write("You're as healthy as a horse")
 
+         elif choice == 'logistic regression':
+             logreg = LogisticRegression()
+             logreg.fit(Xtrain, ytrain)
+             ypred = logreg.predict(heart_data2)
+             if ypred == '0':
+                 st.write("You might have lung cancer, go get a doctor now!)
+             else:
+                 st.write("You're as healthy as a horse")
 
-        with first:
-            st.markdown("**Description**")
-            number1 = data['description']
-            st.markdown(f"<p style=' color: #BAE8E8;'>{number1}</p>", unsafe_allow_html=True)
+         else:
+             nb = GaussianNB()
+             nb.fit(Xtrain, ytrain)
+             ypred = nb.predict(heart_data2)
+             if ypred == '0':
+                 st.write("You might have lung cancer, go get a doctor now!)
+             else:
+                 st.write("You're as healthy as a horse")
 
-        with sec:
-            st.markdown("**License**")
-            st.markdown(f"<p style=' color: #BAE8E8;'>{data['license']}</p>", unsafe_allow_html=True)
-        
-        st.markdown(f"<hr>", unsafe_allow_html=True)
-        language_col=pd.DataFrame(word_counts_for_language)
-        test_set=pd.concat([df,language_col],axis=1)
-        test_set.drop(['tags','description','language','no','most_used_lang'],axis=1,inplace=True)
-        
-        pred=int(model.predict(test_set)[0])
-        if pred>1000:
-            pred=str(float(pred)/1000)+'k'
-        st.success("Your Repo can achieve Popularity  : "+str(pred)+' 🌟')
     
-    except:
-        st.error("Sorry cannot find the repo, please Try again!!!")
