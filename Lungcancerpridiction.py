35312c6ae34f275bc37c3a2f32afa59628b2c9fc
IkmalFazli <102709684+IkmalFazli@users.noreply.github.com>
2022-04-04 16:08:09 +0800

    Update Lungcancerpridiction.py

--- Lungcancerpridiction.py
+++ Lungcancerpridiction.py
@@ -7,6 +7,7 @@ from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
 
 st.title('DID YOU REALLY SAVE FROM LUNG CANCER?')
@@ -26,31 +27,30 @@ choice = st.sidebar.radio(
 )
 
 
-
 form = st.form(key='my_form')
 user_name = form.text_input("Name")
-Age = form.text_input("Age")
-gender= form.text_input("Gender(M for Male, F for Female)")
-smok= form.text_input("Currently smoking?(YES/NO)")
-yf= form.text_input("Yellow Fingers? (YES/NO)")
-Anx= form.text_input("Having anxiety recently?(YES/NO)")
-PP= form.text_input("Pear-pressure?(YES/NO)")
-cd= form.text_input("Any Chronic disease?(YES/NO)")
-Fat= form.text_input("Fatique?(YES/NO)")
-All= form.text_input("Any Allergy reaction?(YES/NO)")
-whe= form.text_input("Wheeing?(YES/NO)")
-Alc= form.text_input("Any alcohol consuming?(YES/NO)")
-cough= form.text_input("Coughing?(YES/NO)")
-Sho= form.text_input("Having Shortness of breath?(YES/NO)")
-swa= form.text_input("Any difficulty to swallow?(YES/NO)")
-cp= form.text_input("Any Chest pain?(YES/NO)")
+A = form.text_input("Gender(M for Male, F for Female)")
+B= form.text_input("Age")
+C= form.text_input("Currently smoking?(YES/NO)")
+D= form.text_input("Yellow Fingers? (YES/NO)")
+E= form.text_input("Having anxiety recently?(YES/NO)")
+F= form.text_input("Pear-pressure?(YES/NO)")
+G= form.text_input("Any Chronic disease?(YES/NO)")
+H= form.text_input("Fatique?(YES/NO)")
+I= form.text_input("Any Allergy reaction?(YES/NO)")
+J= form.text_input("Wheeing?(YES/NO)")
+K= form.text_input("Any alcohol consuming?(YES/NO)")
+L= form.text_input("Coughing?(YES/NO)")
+M= form.text_input("Having Shortness of breath?(YES/NO)")
+N= form.text_input("Any difficulty to swallow?(YES/NO)")
+O= form.text_input("Any Chest pain?(YES/NO)")
 
 submit_button = form.form_submit_button(label='Predict')
 
-if submit_button:
+def prediction(gen, Age, smok, yf, Anx, PP, cd, Fat, All, whe, Alc, cough, Sho, swa, cp):
          heart_data2 = pd.DataFrame(columns = ['GENDER','AGE', 'SMOKING', 'YELLOW_FINGERS', 'ANXIETY', 'PEER_PRESSURE', 'CHRONIC DISEASE', 'FATIGUE', 'ALLERGY', 'WHEEZING',  'ALCOHOL CONSUMING', 'COUGHING', 'SHORTNESS OF BREATH', 'SWALLOWING DIFFICULTY', 'CHEST PAIN' ])
-         heart_data2 = heart_data2.append({'GENDER':gender,'AGE':Age, 'SMOKING':smok, 'YELLOW_FINGERS':yf, 'ANXIETY':Anx, 'PEER_PRESSURE':PP, 'CHRONIC DISEASE':cd, 'FATIGUE':Fat, 'ALLERGY':All, 'WHEEZING':whe,  'ALCOHOL CONSUMING':Alc, 'COUGHING':cough, 'SHORTNESS OF BREATH':sho, 'SWALLOWING DIFFICULTY':swa, 'CHEST PAIN':cp}, ignore_index = True)
-         data = pd.read_csv('survey lung cancer.csv', index_col = False)
+         heart_data2 = heart_data2.append({'GENDER': gen,'AGE':Age, 'SMOKING':smok, 'YELLOW_FINGERS':yf, 'ANXIETY':Anx, 'PEER_PRESSURE':PP, 'CHRONIC DISEASE':cd, 'FATIGUE':Fat, 'ALLERGY':All, 'WHEEZING':whe,  'ALCOHOL CONSUMING':Alc, 'COUGHING':cough, 'SHORTNESS OF BREATH':Sho, 'SWALLOWING DIFFICULTY':swa, 'CHEST PAIN':cp}, ignore_index = True)
+         data = pd.read_csv('C:/Users/Si Kemal/Downloads/survey lung cancer.csv')
          labelencoder1 = LabelEncoder()
          labelencoder2 = LabelEncoder()
          labelencoder3 = LabelEncoder()
@@ -72,8 +72,8 @@ if submit_button:
          data['ANXIETY'] = labelencoder4.fit_transform(data['ANXIETY'])
          data['PEER_PRESSURE'] = labelencoder5.fit_transform(data['PEER_PRESSURE'])
          data['CHRONIC DISEASE'] = labelencoder6.fit_transform(data['CHRONIC DISEASE'])
-         data['FATIGUE '] = labelencoder7.fit_transform(data['FATIGUE '])
-         data['ALLERGY '] = labelencoder8.fit_transform(data['ALLERGY '])
+         data['FATIGUE'] = labelencoder7.fit_transform(data['FATIGUE'])
+         data['ALLERGY'] = labelencoder8.fit_transform(data['ALLERGY'])
          data['WHEEZING'] = labelencoder9.fit_transform(data['WHEEZING'])
          data['ALCOHOL CONSUMING'] = labelencoder10.fit_transform(data['ALCOHOL CONSUMING'])
          data['COUGHING'] = labelencoder11.fit_transform(data['COUGHING'])
@@ -86,10 +86,10 @@ if submit_button:
 
          if choice == 'Random Forest':
              RandomForest = RandomForestClassifier(n_estimators=10000, max_depth=15)
-             RandomForest.fit(Xtrain, ytrain)
+             RandomForest.fit(X, y)
              ypred = RandomForest.predict(heart_data2)
              if ypred == '0':
-                 st.write("You might have lung cancer, go get a doctor now!)
+                 st.write("You might have lung cancer, go get a doctor now!")
              else:
                  st.write("You're as healthy as a horse")
 
@@ -98,26 +98,28 @@ if submit_button:
              knn.fit(X, y)
              ypred = knn.predict(heart_data2)
              if ypred == '0':
-                 st.write("You might have lung cancer, go get a doctor now!)
+                 st.write("You might have lung cancer, go get a doctor now!")
              else:
                  st.write("You're as healthy as a horse")
 
          elif choice == 'logistic regression':
              logreg = LogisticRegression()
-             logreg.fit(Xtrain, ytrain)
+             logreg.fit(X, y)
              ypred = logreg.predict(heart_data2)
              if ypred == '0':
-                 st.write("You might have lung cancer, go get a doctor now!)
+                 st.write("You might have lung cancer, go get a doctor now!")
              else:
                  st.write("You're as healthy as a horse")
 
          else:
              nb = GaussianNB()
-             nb.fit(Xtrain, ytrain)
+             nb.fit(X, y)
              ypred = nb.predict(heart_data2)
              if ypred == '0':
-                 st.write("You might have lung cancer, go get a doctor now!)
+                 st.write("You might have lung cancer, go get a doctor now!")
              else:
                  st.write("You're as healthy as a horse")
 
     
+if submit_button:
+    prediction(A,B,C,D,E,F,G,H,I,J,K,L,M,N,O)
